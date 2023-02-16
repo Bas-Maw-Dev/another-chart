7b6739618b59e1c08c9b70318c343c4ed95b213b
Bas Maw <115073747+Bas-Maw-Dev@users.noreply.github.com>
2023-02-16 14:38:11 +0000

    Test: 2 seperate components render correctly: - pass

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Doughnut from './charts/Doughnut';
+import DoughnutChart from './charts/DoughnutChart';
 function App() {
   return (
     <div className="App">
       <h1>Hello World!</h1>
+      <DoughnutChart />
       <Doughnut />
     </div>
   );
