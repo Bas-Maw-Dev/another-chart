7e64128df19681b754b3c70a36cc9ef6b53536d6
Bas Maw <115073747+Bas-Maw-Dev@users.noreply.github.com>
2023-02-16 14:35:14 +0000

    Test: checking components render : 'Donut' - pass

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
-
+import Doughnut from './charts/Doughnut';
 function App() {
   return (
     <div className="App">
       <h1>Hello World!</h1>
+      <Doughnut />
     </div>
   );
 }
