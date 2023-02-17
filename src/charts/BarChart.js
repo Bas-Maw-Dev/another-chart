5128d51193ec0c5efd979038e33a2cd602d6f533
Bas Maw <115073747+Bas-Maw-Dev@users.noreply.github.com>
2023-02-17 10:25:23 +0000

    tidied up the barchart and it renders correctly under the other element.

--- src/charts/BarChart.js
+++ src/charts/BarChart.js
@@ -1,10 +1,35 @@
 import React from 'react'
-import { Chart as ChartJS} from 'chart.js';
+import { Chart as ChartJS} from 'chart.js/auto';
 import { Bar } from 'react-chartjs-2'
+
+ChartJS.register()
+
+const data = {
+  labels: ['Red', 'Orange', 'Blue'],
+  // datasets is an array of objects where each object represents a set of data to display corresponding to the labels above. for brevity, we'll keep it at one object
+  datasets: [
+      {
+        label: 'Popularity of colours',
+        data: [55, 23, 96],
+        // you can set indiviual colors for each bar
+        backgroundColor: [
+          'rgba(255, 255, 255, 0.6)',
+          'rgba(255, 255, 255, 0.6)',
+          'rgba(255, 255, 255, 0.6)',
+        ],
+        borderWidth: 1,
+      }
+  ]
+}
+
 
 const BarChart = () => {
   return (
-    <div>BarChart</div>
+    <>
+      <div>BarChart</div>
+      <Bar data={data} />
+    </>
+
   )
 }
 
